0bc930a341be0fd659393a528ccec92212eeacc3
dltjsam <dltjsam@gmail.com>
2022-12-15 17:21:32 +0900

    DOM API #수정

--- ch05/src/main/webapp/tabbox02.js
+++ ch05/src/main/webapp/tabbox02.js
@@ -1,7 +1,7 @@
 var onTabClicked = function() {
 	var divTabBox = document.getElementsByClassName("tab-box")[0];
 	var ul = divTabBox.childNodes[1];
-	lis = ul.getElementsByClassName('selected');
+	var lis = ul.getElementsByClassName('selected');
 	
 	(lis.length == 1) && (lis[0].className = "");
 	
